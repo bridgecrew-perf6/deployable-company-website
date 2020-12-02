7e607891222abfcf9c370bda63cb5584e7f8eb31
imbenrabi <imbenrabi@gmail.com>
2020-12-02 16:37:39 +0200

    title and favicon

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <title>Deployable.</title>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
